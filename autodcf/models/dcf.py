3d90c7f1001d8fb7c524367b32d6d80ad29993dd
jackmoody11 <jackmoody@unc.edu>
2020-01-29 01:00:34 -0500

    CLEAN: Flake8

--- autodcf/models/dcf.py
+++ autodcf/models/dcf.py
@@ -15,6 +15,11 @@ def multiply_by_sales_percent(df, percent_of_sales):
     return df['Sales'] * percent_of_sales
 
 
+def calculate_free_cash_flow(df):
+    tax_rates = (df['Taxes'] / df['EBT'])
+    return df['EBITDA'] * tax_rates + df['D&A'] * tax_rates - df['Capex'] - df['Change in NWC']
+
+
 def discount_cash_flows(cash_flows, discount_rate):
     """Discount cash flows at given discount rate."""
     discount_factors = np.array([1 / (1 + discount_rate) ** i for i in range(len(cash_flows))])
@@ -79,8 +84,9 @@ class DCF(AbstractDCF):
 
     def forecast(self):
         """Get pandas dataframe with all info needed to complete forecast."""
-        self._forecast['Period'] = np.arange(-1, self.window + 1)
-        self._forecast['Year'] = np.arange(datetime.now().year, datetime.now().year + self.window + 1)
+        self._forecast.index = np.arange(-1, self.window + 1)
+        self._forecast['Year'] = np.arange(datetime.now().year,
+                                           datetime.now().year + self.window + 1)
         self._forecast['Sales'] = fill_row(self.company.income_statement.sales, self._sales_growth)
         self._forecast['COGS'] = multiply_by_sales_percent(self._forecast, self._cogs_to_sales)
         self._forecast['Gross Profit'] = self._forecast['Sales'] - self._forecast['COGS']
@@ -89,17 +95,17 @@ class DCF(AbstractDCF):
         self._forecast['R&D'] = multiply_by_sales_percent(self._forecast, self._rd_to_sales)
         self._forecast['EBITDA'] = self._forecast['Operating Profit'] - self._forecast['R&D']
         self._forecast['D&A'] = multiply_by_sales_percent(self._forecast, self._da_to_sales)
-        self._forecast['EBIT'] = self._forecast['EBITDA'] - self._forecast['D&A']
-        self._forecast['Interest'] = multiply_by_sales_percent(self._forecast, self._interest_to_sales)
+        self._forecast['EBIT'] = self._forecast['EBITDA'] - self._forecast['D&A']  # noqa:E501
+        self._forecast['Interest'] = multiply_by_sales_percent(self._forecast,
+                                                               self._interest_to_sales)
         self._forecast['EBT'] = self._forecast['EBIT'] - self._forecast['Interest']
         self._forecast['Taxes'] = self._forecast['EBT'] * self._tax_rate
         self._forecast['Net Income'] = self._forecast['EBT'] - self._forecast['Taxes']
         self._forecast['Capex'] = multiply_by_sales_percent(self._forecast, self._capex_to_sales)
         # ΔSales * ΔNWC/ΔSales = ΔNWC
-        self._forecast['Change in NWC'] = np.diff(self._forecast['Sales']) * self._change_in_nwc_to_change_in_sales
-        tax_rates = (self._forecast['Taxes'] / self._forecast['EBT'])
-        self._forecast['Free Cash Flow'] = self._forecast['EBITDA'] * tax_rates + self._forecast['D&A'] * tax_rates - \
-                                           self._forecast['Capex'] - self._forecast['Change in NWC']
-        self._forecast['Discounted Free Cash Flow'] = discount_cash_flows(self._forecast['Free Cash Flow'],
-                                                                          self._discount_rate)
+        change_in_sales = np.diff(self._forecast['Sales'])
+        self._forecast['Change in NWC'] = change_in_sales * self._change_in_nwc_to_change_in_sales
+        self._forecast['FCF'] = calculate_free_cash_flow(self._forecast)
+        self._forecast['Discounted FCF'] = discount_cash_flows(self._forecast['FCF'],
+                                                               self._discount_rate)
         return self._forecast
